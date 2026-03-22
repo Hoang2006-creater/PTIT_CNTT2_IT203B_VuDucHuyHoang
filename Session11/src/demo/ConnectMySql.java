72d73a6f4b79229734f360cae444709cfc4ed560
Vũ Đức Huy Hoàng <duhoang1611@gmail.com>
2026-03-22 20:45:14 +0700

    Nopbaitap

--- Session11/src/demo/ConnectMySql.java
+++ Session11/src/demo/ConnectMySql.java
@@ -1,4 +1,28 @@
 package demo;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectMySql {
+    public static Connection ConnectMySql() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost3306/my_db1","admin","1234");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ConnectMySql());
+    }
 }
